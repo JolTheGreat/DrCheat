90195b1cf53e3ccbb1c31d06d7901a2ee7a4c02e
JolTheGreat <katoshin0805@gmail.com>
2022-10-28 12:05:47 +0100

    Optimize

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ console.log = function () {
 const intervalId = window.setInterval(function () {
     if (console.logs.length > 0) {
         let last = console.logs[console.logs.length - 1].toString();
-        if (last !== undefined && last.startsWith("QUESTION SKILL: ")) {
+        if (last !== undefined && last.startsWith("QUESTION SKILL: ") && !last.startsWith("DrCheat: ")) {
             const json = JSON.parse(last.substring(16));
             alert(JSON.stringify(json.answer) + "\n Refer to the console by pressing F12 for more information.");
-            console.log(json);
         }
         console.logs.pop();
 
